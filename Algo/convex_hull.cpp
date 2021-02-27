cc06cac1304313fe6aa99603b91e403cecdc6cbc
João Victor Malheiros Farias de Aquino <38818400+Malhe1ros@users.noreply.github.com>
2021-02-27 14:28:45 -0300

    Add files via upload

--- Algo/convex_hull.cpp
+++ Algo/convex_hull.cpp
@@ -5,12 +5,6 @@ using namespace std;
 #define pii pair<usando,usando>
 #define x first
 #define y second
-/*
-    Dado n pontos, fazer o polar sort counterclockwise em relação a um deles
-    primeiro em relação ao angulo depois em relação a distancia
-    (1,0),(0,1),(0,0),(-1,0),(2,0),(0,-1) em relação  a (0,0) -> (0,0),(1,0),(2,0),(0,1),(-1,0),(0,-1)
-*/
-pii ponto_fixo;
 //cross product do vetor A->B com A->C
 //B_aXC_a
 //cross product positivo se C estiver a esquerda de B
@@ -21,32 +15,65 @@ usando cross_product(pii&a,pii&b,pii&c){
     usando yb=b.y-a.y;
     return xc*yb-xb*yc;
 }
-bool cmp(pii& a,pii& b){
-    if (a.y>=ponto_fixo.y && b.y<ponto_fixo.y) return true;
-    if (a.y<ponto_fixo.y && b.y>=ponto_fixo.y) return false;
-    usando cp=cross_product(ponto_fixo,a,b);
-    if (cp==0){
-        return ((a.x-ponto_fixo.x)*(a.x-ponto_fixo.x)+(a.y-ponto_fixo.y)*(a.y-ponto_fixo.y))<((b.x-ponto_fixo.x)*(b.x-ponto_fixo.x)+(b.y-ponto_fixo.y)*(b.y-ponto_fixo.y));
+
+
+
+vector<pii> convex_hull(vector<pii> pontos){
+    sort(pontos.begin(),pontos.end());
+    vector<pii> ans;
+    ans.push_back(pontos[0]);
+        //for (auto k:pontos) cout<<"("<<k.first<<" "<<k.second<<") , ";
+        //cout<<endl;
+    for (int i=1;i<pontos.size();i++){
+        //for (auto k:ans) cout<<"("<<k.first<<" "<<k.second<<") , ";
+        //cout<<endl;
+        if (ans.size()==1 || cross_product(ans[ans.size()-2],ans.back(),pontos[i])<0){
+            if(pontos[i]!=ans.back())ans.push_back(pontos[i]);
+        }
+        else{
+            while(ans.size()>=2 && cross_product(ans[ans.size()-2],ans.back(),pontos[i])>=0){
+                ans.pop_back();
+            }
+            if(pontos[i]!=ans.back())ans.push_back(pontos[i]);
+        }
     }
-    return cp<0;
+        //for (auto k:ans) cout<<"("<<k.first<<" "<<k.second<<") , ";
+        //cout<<endl;
+    vector<pii> helper;
+    helper.push_back(pontos[0]);
+        for (int i=1;i<pontos.size();i++){
+        //for (auto k:ans) cout<<"("<<k.first<<" "<<k.second<<") , ";
+        //cout<<endl;
+        if (helper.size()==1 || cross_product(helper[helper.size()-2],helper.back(),pontos[i])>0){
+            if(pontos[i]!=helper.back())helper.push_back(pontos[i]);
+        }
+        else{
+            while(helper.size()>=2 && cross_product(helper[helper.size()-2],helper.back(),pontos[i])<=0){
+                helper.pop_back();
+            }
+            if(pontos[i]!=helper.back())helper.push_back(pontos[i]);
+        }
+    }
+    int n=helper.size();
+    for (int i=n-2;i>=1;i--){
+        ans.push_back(helper[i]);
+    }
+    return ans;
 }
-// ordena os pontos em relacao ao pontos[index] em counterclocwise order;
-void polar_sort(vector<pii>& pontos,int index){
-    ponto_fixo=pontos[index];
-    swap(pontos[0],pontos[index]);
-    sort(++pontos.begin(),pontos.end(),cmp);
-}
-
-
 int main(){
-   int t;
-   cin>>t;
-       vector<pii> pontos;
-   while(t--){
-       int a,b;
-       cin>>a>>b;
-
+    while(1){
+    int t;
+    cin>>t;
+    if (t==0) break;
+    vector<pii> pontos;
+    while(t--){
+        int a,b;cin>>a>>b;
         pontos.push_back({a,b});
-   }
-   polar_sort(pontos,1);
+    }
+    
+    vector<pii> ans = convex_hull(pontos);
+    cout<<ans.size()<<endl;
+    for (auto k:ans)cout<<k.first<<" "<<k.second<<endl;
+    //cout<<endl;
+    }
 }
