cf53d7a28b0aba20927ef0f059b6dc5551007c4d
João Victor Malheiros Farias de Aquino <38818400+Malhe1ros@users.noreply.github.com>
2021-04-12 19:47:16 -0300

    Update convex_hull.cpp

--- Algo/convex_hull.cpp
+++ Algo/convex_hull.cpp
@@ -1,79 +1,126 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define usando double
-#define pii pair<usando,usando>
-#define x first
-#define y second
-//cross product do vetor A->B com A->C
-//B_aXC_a
-//cross product positivo se C estiver a esquerda de B
-usando cross_product(pii&a,pii&b,pii&c){
-    usando xc=c.x-a.x;
-    usando yc=c.y-a.y;
-    usando xb=b.x-a.x;
-    usando yb=b.y-a.y;
-    return xc*yb-xb*yc;
+/*
+How to use:
+Create a vector of points and pass it to the convex_hull function
+it will return the convex hull of the points (without equal and collinear points)[change sign to add collinear]
+*/
+//Can be used to solve https://open.kattis.com/submissions/7141168
+
+struct Point{
+	int x,y;
+	Point(){
+
+	}
+	Point(int a,int b){
+		x=a;
+		y=b;
+	}
+	bool operator< (const Point & other){
+		if (other.x==x)return y<other.y;
+		return x<other.x;
+	}
+	bool operator!= (const Point & other){
+		return other.x != x || other.y!=y;
+	}
+};
+struct Vetor{
+	int x,y;
+	//creates vector from a to b
+	Vetor(Point a,Point b){
+		x=b.x-a.x;
+		y=b.y-a.y;
+	}
+	int cross(Vetor other){
+		return x*other.y-y*other.x;
+	}
+};
+/*
+Creates vector from A to B and from A to C
+Return:
+negative if A->B to the left of A->C;
+zero if collinear;
+positive if A->B to the right of A->C;
+*/
+int cross(Point a,Point b,Point c){
+	return Vetor(a,b).cross(Vetor(a,c));
+}
+
+/*
+creates the upper hull of the points
+*/
+
+vector<Point> upper_hull(vector<Point>& pontos){
+	vector<Point> ans;
+	ans.push_back(pontos[0]);
+	for (int i=1;i<pontos.size();i++){
+		//to avoid adding equal points
+
+		if (ans.size()==1 || cross(ans[ans.size()-2],ans.back(),pontos[i])<0){
+			if (pontos[i]!=ans.back())ans.push_back(pontos[i]);
+		}
+		else{
+			// >= to avoid adding colinear points
+			while(ans.size()>=2 && cross(ans[ans.size()-2],ans.back(),pontos[i])>=0){
+				ans.pop_back();
+			}
+			// To avoid adding duplicates
+			if (pontos[i]!=ans.back())ans.push_back(pontos[i]);
+		}
+	}
+	return ans;
+}
+
+/*
+creates the down hull of the points;
+*/
+
+vector<Point> down_hull(vector<Point>& pontos){
+	vector<Point> ans;
+	ans.push_back(pontos[0]);
+	for (int i=1;i<pontos.size();i++){
+		if (ans.size()==1 || cross(ans[ans.size()-2],ans.back(),pontos[i])>0){
+			//to avoid adding equal points
+			if (pontos[i]!=ans.back())ans.push_back(pontos[i]);
+		}
+		else{
+			// >= to avoid adding colinear points
+			while(ans.size()>=2 && cross(ans[ans.size()-2],ans.back(),pontos[i])<=0){
+				ans.pop_back();
+			}
+			// To avoid adding duplicates
+			if (pontos[i]!=ans.back())ans.push_back(pontos[i]);
+		}
+	}
+	return ans;
 }
 
 
 
-vector<pii> convex_hull(vector<pii> pontos){
+
+vector<Point> convex_hull(vector<Point>& pontos){
     sort(pontos.begin(),pontos.end());
-    vector<pii> ans;
-    ans.push_back(pontos[0]);
-        //for (auto k:pontos) cout<<"("<<k.first<<" "<<k.second<<") , ";
-        //cout<<endl;
-    for (int i=1;i<pontos.size();i++){
-        //for (auto k:ans) cout<<"("<<k.first<<" "<<k.second<<") , ";
-        //cout<<endl;
-        if (ans.size()==1 || cross_product(ans[ans.size()-2],ans.back(),pontos[i])<0){
-            if(pontos[i]!=ans.back())ans.push_back(pontos[i]);
-        }
-        else{
-            while(ans.size()>=2 && cross_product(ans[ans.size()-2],ans.back(),pontos[i])>=0){
-                ans.pop_back();
-            }
-            if(pontos[i]!=ans.back())ans.push_back(pontos[i]);
-        }
+    auto uh=upper_hull(pontos);
+    auto dh=down_hull(pontos);
+    vector<Point> ansf;
+    for (int i=0;i<dh.size();i++){
+        ansf.push_back(dh[i]);
     }
-        //for (auto k:ans) cout<<"("<<k.first<<" "<<k.second<<") , ";
-        //cout<<endl;
-    vector<pii> helper;
-    helper.push_back(pontos[0]);
-        for (int i=1;i<pontos.size();i++){
-        //for (auto k:ans) cout<<"("<<k.first<<" "<<k.second<<") , ";
-        //cout<<endl;
-        if (helper.size()==1 || cross_product(helper[helper.size()-2],helper.back(),pontos[i])>0){
-            if(pontos[i]!=helper.back())helper.push_back(pontos[i]);
-        }
-        else{
-            while(helper.size()>=2 && cross_product(helper[helper.size()-2],helper.back(),pontos[i])<=0){
-                helper.pop_back();
-            }
-            if(pontos[i]!=helper.back())helper.push_back(pontos[i]);
-        }
-    }
-    int n=helper.size();
+    int n=uh.size();
     for (int i=n-2;i>=1;i--){
-        ans.push_back(helper[i]);
+        ansf.push_back(uh[i]);
     }
-    return ans;
+    return ansf;
 }
 int main(){
-    while(1){
-    int t;
-    cin>>t;
-    if (t==0) break;
-    vector<pii> pontos;
-    while(t--){
-        int a,b;cin>>a>>b;
-        pontos.push_back({a,b});
-    }
-    
-    vector<pii> ans = convex_hull(pontos);
-    cout<<ans.size()<<endl;
-    for (auto k:ans)cout<<k.first<<" "<<k.second<<endl;
-    //cout<<endl;
-    }
+	while(1){
+		int n;cin>>n;
+		if (n==0)break;
+		vector<Point>v(n);
+		for (auto &k:v){
+			cin>>k.x>>k.y;
+		}
+		convex_hull(v);
+	}
 }
