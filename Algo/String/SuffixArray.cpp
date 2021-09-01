e266f34e70b3fc8955692b0c2a7b6c1da18c423b
João Victor Malheiros Farias de Aquino <38818400+Malhe1ros@users.noreply.github.com>
2021-09-01 16:51:50 -0300

    Update SuffixArray.cpp

--- Algo/String/SuffixArray.cpp
+++ Algo/String/SuffixArray.cpp
@@ -1,31 +1,46 @@
 #include<bits/stdc++.h>
-
+ 
 using namespace std;
-
+ 
 struct SuffixArray{
     string s;
     int n;
     vector<int> arr;
+    vector<int>pos;
+    vector<int> LCP;
     SuffixArray(string& _s){
         s=_s+'$';
         n=s.length();
+        pos=vector<int>(n);
         arr=vector<int>(n);
+        LCP=vector<int>(n);
         build();
+        kasai();
     }
 
+    void kasai(){
+        int k=0;
+        for (int i=0;i<n-1;i++){
+            int j=arr[pos[i]-1];
+            while(s[i+k]==s[j+k])k++;
+            LCP[pos[i]]=k;
+            k=max(k-1,0);
+        }
+    }
+    
     void counting_sort(vector<int>& p,vector<int>&c){
         vector<int>cnt(n);
         for (auto k:c) cnt[k]++;
     
         vector<int>pref(n);
         for (int i=1;i<n;i++) pref[i]=pref[i-1]+cnt[i-1];
-
+ 
         vector<int> ans(n);
         for (auto k:p) ans[pref[c[k]]++]=k;
         
         p=ans;   
     }
-
+ 
     void build(){
         vector<pair<char,int>> pares(n);
         for(int i=0;i<n;i++)pares[i]={s[i],i};
@@ -42,13 +57,13 @@ struct SuffixArray{
         }
         int k=0;
         while((1<<k) < n){
-
+ 
             for (int i=0;i<n;i++){
                 p[i]=(p[i]-(1<<k)+n)%n;
             }
             
             counting_sort(p,eqclass);
-
+ 
             vector<int>neweqclass(n);
             neweqclass[p[0]]=0;
             for (int i=1;i<n;i++){
@@ -61,14 +76,58 @@ struct SuffixArray{
             k++;
         }
         for (int i=0;i<n;i++)arr[i]=p[i];
+        for (int i=0;i<n;i++)pos[arr[i]]=i;
+        
     }
+    /*
+    int pode(string&pat,int ind){
+        for (int i=ind;i<ind+pat.size();i++){
+            if (i>=n)return -1;
+            if (pat[i-ind]>s[i])return 1;
+            if (pat[i-ind]<s[i])return -1;
+        }
+        return 0;
+    }
+    int qntd_subst(string &pat){
+        int l=0;
+        int r=n;
+        bool achei=0;
+        while(l<r){
+            int mid=(l+r)/2;
+            auto teste=pode(pat,arr[mid]);
+            if (teste==0)achei=1;
+            if (teste>=0) l=mid+1;
+            else r=mid;
+        }
+        if (!achei)return 0;
+        int maxr=r;
+        l=0;r=n;
+        while(l<r){
+            int mid=(l+r)/2;
+            auto teste=pode(pat,arr[mid]);
+            if (teste==0)achei=1;
+            if (teste>0) l=mid+1;
+            else r=mid;
+        }
+        return maxr-r;
+    }
+    */
+ 
 };
-
-
+ 
+ 
 int main(){
     cin.tie(NULL);
     cin.sync_with_stdio(false);
     string s;cin>>s;
     SuffixArray sa(s);
-
+    for (auto k:sa.arr){
+        cout<<k<<" ";
+    }
+    cout<<'\n';
+    for (int i=1;i<1+s.length();i++){
+        cout<<sa.LCP[i]<<" ";
+    }
+    cout<<endl;
+    
 }
