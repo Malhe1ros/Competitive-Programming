505441d4cfd984c8d8665583118ab0a0b5b88a24
João Victor Malheiros Farias de Aquino <38818400+Malhe1ros@users.noreply.github.com>
2021-12-09 13:16:15 -0300

    Create 2SAT.cpp

---
+++ Algo/Graph/SCC + 2SAT/2SAT.cpp
@@ -0,0 +1,106 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+//solves https://cses.fi/problemset/task/1684/
+
+// 2-SAT (2-satisfiability problem)
+// given boolean variables on the form (a or b) and (c or d) and... (y or z)
+// is there a true/false assignment for each variable such that all constraints are satisfied?
+//
+// solve() returns a pair
+// first = is possible?
+// second =  vector[i] = 1 if i is true;
+// Done in linear time
+
+struct TWOsat {
+	int n, tot;
+	vector<vector<int>> g;
+	vector<int> vis, comp, id, ans;
+	stack<int> s;
+
+	TWOsat() {}
+	TWOsat(int n_) : n(n_), tot(n), g(2*n) {}
+
+	int dfs(int i, int& t) {
+		int lo = id[i] = t++;
+		s.push(i), vis[i] = 2;
+		for (int j : g[i]) {
+			if (!vis[j]) lo = min(lo, dfs(j, t));
+			else if (vis[j] == 2) lo = min(lo, id[j]);
+		}
+		if (lo == id[i]) while (1) {
+			int u = s.top(); s.pop();
+			vis[u] = 1, comp[u] = i;
+			if ((u>>1) < n and ans[u>>1] == -1) ans[u>>1] = ~u&1;
+			if (u == i) break;
+		}
+		return lo;
+	}
+
+  //All of these operations translates to x ou y in some way;
+
+	void add_impl(int x, int y) { // x -> y = !x ou y
+		x = x >= 0 ? 2*x : -2*x-1;
+		y = y >= 0 ? 2*y : -2*y-1;
+		g[x].push_back(y);
+		g[y^1].push_back(x^1);
+	}
+	void add_or(int x, int y) { // x ou y
+		add_impl(~x, y);
+	}
+	void add_xor(int x, int y) { // x xor y
+		add_or(x, y), add_or(~x, ~y);
+	}
+	void add_eq(int x, int y) { // x = y
+		add_xor(~x, y);
+	}
+	void add_true(int x) { // x = T
+		add_impl(~x, x);
+	}
+	/*
+  void at_most_one(vector<int> v) { // no max um verdadeiro
+		g.resize(2*(tot+v.size()));
+		for (int i = 0; i < v.size(); i++) {
+			add_impl(tot+i, ~v[i]);
+			if (i) {
+				add_impl(tot+i, tot+i-1);
+				add_impl(v[i], tot+i-1);
+			}
+		}
+		tot += v.size();
+	}
+  */
+	pair<bool, vector<int>> solve() {
+		ans = vector<int>(n, -1);
+		int t = 0;
+		vis = comp = id = vector<int>(2*tot, 0);
+		for (int i = 0; i < 2*tot; i++) if (!vis[i]) dfs(i, t);
+		for (int i = 0; i < tot; i++)
+			if (comp[2*i] == comp[2*i+1]) return {false, {}};
+		return {true, ans};
+	}
+};
+
+signed main(){
+  int a,b;cin>>a>>b;
+  TWOsat s(b+1);
+  while(a--){
+    char c,d;
+    int x,y;
+    cin>>c>>x>>d>>y;
+    if (c=='-')x=~x;
+    if (d=='-')y=~y;
+    s.add_or(x,y);
+  }
+  auto ans=s.solve();
+  
+  if (ans.first){
+    for(int i=1;i<=b;i++){
+      if(ans.second[i])cout<<"+ ";
+      else cout<<"- ";
+    }
+    cout<<'\n';
+  }
+  else cout<<"IMPOSSIBLE\n";
+}
