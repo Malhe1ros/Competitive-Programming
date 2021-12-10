bbc8b05ba93c07181f03de5c036162191a25a327
João Victor Malheiros Farias de Aquino <38818400+Malhe1ros@users.noreply.github.com>
2021-12-10 16:35:16 -0300

    Update FFT and NTT.cpp

--- Algo/Polynomial/FFT and NTT.cpp
+++ Algo/Polynomial/FFT and NTT.cpp
@@ -1,51 +1,13 @@
+
 #include <bits/stdc++.h>
 using namespace std;
 
-//ONLY C++17
-using ll = long long;
-using db = long double; // or double, if TL is tight
-using str = string; 
-
-using vl = vector<ll>;
-using vi = vector<int>;
-
-#define tcT template<class T
-#define tcTU tcT, class U
-tcT> using V = vector<T>;
-tcT, size_t SZ> using AR = array<T,SZ>;
-tcT> using PR = pair<T,T>;
-
-// pairs
-#define mp make_pair
-#define f first
-#define s second
-
 #define sz(x) int((x).size())
 
-// loops
-#define FOR(i,a,b) for (int i = (a); i < (b); ++i)
-#define F0R(i,a) FOR(i,0,a)
-#define ROF(i,a,b) for (int i = (b)-1; i >= (a); --i)
-#define R0F(i,a) ROF(i,0,a)
-#define each(a,x) for (auto& a: x)
-
-// INPUT
-#define tcTUU tcT, class ...U
-tcT> void re(T& x) { cin >> x; }
-tcTUU> void re(T& t, U&... u) { re(t); re(u...); }
-tcT> void re(V<T>& x) { each(a,x) re(a); }
-
-void setPrec() { cout << fixed << setprecision(15); }
-void unsyncIO() { cin.tie(0)->sync_with_stdio(0); }
-void setIO() {
-	unsyncIO(); setPrec();
-}
-
-#define rep(i, a, b) for(int i = a; i < (b); ++i)
-typedef pair<int, int> pii;
-
-
 typedef complex<double> C;
+
+#define ll long long
+
 void fft(vector<C>& a) {
 	int n = sz(a), L = 31 - __builtin_clz(n);
 	static vector<complex<long double>> R(2, 1);
@@ -53,13 +15,13 @@ void fft(vector<C>& a) {
 	for (static int k = 2; k < n; k *= 2) {
 		R.resize(n); rt.resize(n);
 		auto x = polar(1.0L, acos(-1.0L) / k);
-		rep(i,k,2*k) rt[i] = R[i] = i&1 ? R[i/2] * x : R[i/2];
+		for(int i=k;i<2*k;i++) rt[i] = R[i] = i&1 ? R[i/2] * x : R[i/2];
 	}
-	vi rev(n);
-	rep(i,0,n) rev[i] = (rev[i / 2] | (i & 1) << L) / 2;
-	rep(i,0,n) if (i < rev[i]) swap(a[i], a[rev[i]]);
+	vector<int> rev(n);
+	for(int i=0;i<n;i++) rev[i] = (rev[i / 2] | (i & 1) << L) / 2;
+	for(int i=0;i<n;i++) if (i < rev[i]) swap(a[i], a[rev[i]]);
 	for (int k = 1; k < n; k *= 2)
-		for (int i = 0; i < n; i += 2 * k) rep(j,0,k) {
+		for (int i = 0; i < n; i += 2 * k) for(int j=0;j<k;j++) {
 			// C z = rt[j+k] * a[i+j+k]; // (25% faster if hand-rolled)  /// include-line
 			auto x = (double *)&rt[j+k], y = (double *)&a[i+j+k];        /// exclude-line
 			C z(x[0]*y[0] - x[1]*y[1], x[0]*y[1] + x[1]*y[0]);           /// exclude-line
@@ -68,57 +30,43 @@ void fft(vector<C>& a) {
 		}
 }
 
-typedef vector<ll> vl;
-template<int M> vl convMod(const vl &a, const vl &b) {
+template<int M> 
+vector<ll> convMod(const vector<ll> &a, const vector<ll> &b) {
 	if (a.empty() || b.empty()) return {};
-	vl res(sz(a) + sz(b) - 1);
+	vector<ll> res(sz(a) + sz(b) - 1);
 	int B=32-__builtin_clz(sz(res)), n=1<<B, cut=int(sqrt(M));
 	vector<C> L(n), R(n), outs(n), outl(n);
-	rep(i,0,sz(a)) L[i] = C((int)a[i] / cut, (int)a[i] % cut);
-	rep(i,0,sz(b)) R[i] = C((int)b[i] / cut, (int)b[i] % cut);
+	for(int i=0;i<sz(a);i++) L[i] = C((int)a[i] / cut, (int)a[i] % cut);
+	for(int i=0;i<sz(b);i++) R[i] = C((int)b[i] / cut, (int)b[i] % cut);
 	fft(L), fft(R);
-	rep(i,0,n) {
+	for(int i=0;i<n;i++){
 		int j = -i & (n - 1);
 		outl[j] = (L[i] + conj(L[j])) * R[i] / (2.0 * n);
 		outs[j] = (L[i] - conj(L[j])) * R[i] / (2.0 * n) / 1i;
 	}
 	fft(outl), fft(outs);
-	rep(i,0,sz(res)) {
-		ll av = ll(real(outl[i])+.5), cv = ll(imag(outs[i])+.5);
-		ll bv = ll(imag(outl[i])+.5) + ll(real(outs[i])+.5);
+	for(int i=0;i<sz(res);i++){
+		ll av = (ll)(real(outl[i])+.5), cv = (ll)(imag(outs[i])+.5);
+		ll bv = (ll)(imag(outl[i])+.5) + (ll)(real(outs[i])+.5);
 		res[i] = ((av % M * cut + bv) % M * cut + cv) % M;
 	}
 	return res;
 }
 
+const ll MOD=998244353;
 int main() {
-	setIO();
-  int trans = 30010;
-  int as; cin >> as;
-  vl a(2 * trans), c(2 * trans), b(4 * trans);
-  for(int i = 0; i < as; ++i){
-    int x; cin >> x; a[x + trans]++;
+	int n,m;cin>>n>>m;
+  vector<ll>v1(n);
+  vector<ll>v2(m);
+  for(int i=0;i<n;i++){
+    cin>>v1[i];
   }
-
-  int bs; cin >> bs;
-  for(int i = 0; i < bs; ++i){
-    int x; cin >> x; b[(x + trans) * 2]++;
+  for(int i=0;i<m;i++){
+    cin>>v2[i];
   }
-
-  int cs; cin >> cs;
-  for(int i = 0; i < cs; ++i){
-    int x; cin >> x; c[x + trans]++;
+  auto ans=convMod<MOD>(v1,v2);
+  for(auto k:ans){
+    cout<<k<<" ";
   }
-
-
-  auto ret = convMod<1000000000>(a, c);
-
-  int ans = 0;
-  for(int i = 0; i < ret.size(); ++i){
-    if(ret[i] && b[i]){
-      ans += ret[i];
-      // cout << i << " ";
-    }
-  }
-  cout << ans << endl;
+  cout<<'\n';
 }
