22c82cd6b0c61082c586830007ddb9994f7e703d
João Victor Malheiros Farias de Aquino <38818400+Malhe1ros@users.noreply.github.com>
2022-03-03 12:02:38 -0300

    Update DSU.cpp

--- DS/DSU/DSU.cpp
+++ DS/DSU/DSU.cpp
@@ -2,14 +2,12 @@
 
 using namespace std;
 
+
 struct DSU{
     int* arr;
     DSU(int size){
         arr = new int[size];
-        
-        for (int i=0;i<size;i++){
-            arr[i]=-1;
-        }
+		for(int i=0;i<size;i++)arr[i]=-1;
     }
     int find(int a){
         int ac=a;
@@ -21,57 +19,31 @@ struct DSU{
             arr[ac]=a;
             ac=cop;
         }
-
         return a;
     }
     void unir(int a,int b){
         int paia=find(a);
         int paib=find(b);
         if (paia==paib) return;
-        int tam=arr[paia]+arr[paib];
-        if (arr[paia]>arr[paib]){
-            arr[paia]=paib;
-            arr[paib]=tam;
-        }
-        else{
-            arr[paib]=paia;
-            arr[paia]=tam;
-        }
+		if(arr[paia]>arr[paib])swap(paia,paib);
+        arr[paia]+=arr[paib];
+		arr[paib]=paia;
     }
 };
 
-
-
-int main(){
-    DSU d = DSU(8);
-
-    d.unir(0,1);
-
-    d.unir(2,3);
-
-    d.unir(4,5);
-
-    d.unir(1,4);
-
-    d.unir(0,1);
-    for (int i=0;i<8;i++){
-        cout<<d.arr[i]<<" ";
-    }
-    cout<<endl;
-    d.unir(0,3);
-    for (int i=0;i<8;i++){
-        cout<<d.arr[i]<<" ";
-    }
-    cout<<endl;
-    d.unir(0,7);
-    for (int i=0;i<8;i++){
-        cout<<d.arr[i]<<" ";
-    }
-    cout<<endl;
-    d.unir(6,7);
-    for (int i=0;i<8;i++){
-        cout<<d.arr[i]<<" ";
-    }
-    cout<<endl;
-
+signed main(){
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	int n,q;cin>>n>>q;
+	DSU dsu(n);
+	while(q--){
+		int a,b,c;cin>>a>>b>>c;
+		//cout<<b<<" "<<c<<endl;
+		if(a){
+			cout<<(dsu.find(b)==dsu.find(c))<<'\n';
+		}
+		else{
+			dsu.unir(b,c);
+		}
+	}
 }
