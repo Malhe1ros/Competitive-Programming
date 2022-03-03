5a0c561f2790a4139250ea3f4477961d059e54eb
João Victor Malheiros Farias de Aquino <38818400+Malhe1ros@users.noreply.github.com>
2022-03-03 11:49:38 -0300

    Update Sparse_table.cpp

--- DS/Sparse_table.cpp
+++ DS/Sparse_table.cpp
@@ -1,34 +1,48 @@
-    #include <bits/stdc++.h>
-    
-    using namespace std;
-    
-    
-    class sparse{
-        public:
-        vector<vector<int>> indo;
-        sparse(vector<int>& arr){
-            int k=32-__builtin_clz(arr.size());
-            indo.reserve(k+1);
-            for (int i=0;i<k;i++){
-                indo.push_back(vector<int>(arr.size()));
-            }
-            for (int i=0;i<arr.size();i++){
-                indo[0][i]=arr[i];   
-            }
-            int t=1;
-            for (int i=1;i<k;i++){
-                
-                for (int j=0;j<=arr.size()-(1<<i);j++){
-                    indo[i][j]=min(indo[i-1][j],indo[i-1][j+t]);
-                }
-                t=(t<<1);
-            }
-        }
-        int query(int a,int b){
-            b++;
-            int d=b-a;
-            int k=31-__builtin_clz(d);
-            return min(indo[k][a],indo[k][b-(1<<k)]);
-        }
-    };
+#include <bits/stdc++.h>
 
+using namespace std;
+
+
+struct sparse{
+	vector<vector<int>> indo;
+	int f(int a,int b){
+		return min(a,b);
+	}
+	sparse(vector<int>& arr){
+		int n=arr.size();
+		int k=32-__builtin_clz(n);
+		//indo.reserve(k+1);
+		indo.assign(k+1,vector<int>(n));
+		for (int i=0;i<arr.size();i++){
+			indo[0][i]=arr[i];   
+		}
+		int t=1;
+		for (int i=1;i<k;i++){
+			for (int j=0;j<=arr.size()-(1<<i);j++){
+				indo[i][j]=f(indo[i-1][j],indo[i-1][j+t]);
+			}
+			t=(t<<1);
+		}
+	}
+	int query(int a,int b){//[a,b[
+		//b++;
+		int d=b-a;
+		int k=31-__builtin_clz(d);
+		return f(indo[k][a],indo[k][b-(1<<k)]);
+	}
+};
+
+signed main(){
+	int n,q;cin>>n>>q;
+	vector<int>v(n);
+	for(int i=0;i<n;i++){
+		cin>>v[i];
+		//cout<<v[i]<<endl;
+	}	
+	sparse st(v);
+	while(q--){
+		int a,b;cin>>a>>b;
+		//cout<<a<<" "<<b<<endl;
+		cout<<st.query(a,b)<<'\n';
+	}
+}
