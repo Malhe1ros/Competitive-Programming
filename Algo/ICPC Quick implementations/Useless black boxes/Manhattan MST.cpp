0d21cdb0cbb1b1a9d5a902a458774ff61b0c3a2a
João Victor Malheiros Farias de Aquino <38818400+Malhe1ros@users.noreply.github.com>
2022-03-24 10:25:18 -0300

    Update Manhattan MST.cpp

--- Algo/ICPC Quick implementations/Useless black boxes/Manhattan MST.cpp
+++ Algo/ICPC Quick implementations/Useless black boxes/Manhattan MST.cpp
@@ -1,117 +1,93 @@
-/*
-We are given a CNF, e.g. phi(x) = (x_1 or ~x_2) and (x_3 or ~x_4 or ~x_5) and ... .
-SAT finds an assignment x for phi(x) = true.
-Davis-Putnum-Logemann-Loveland Algorithm
-Complexity: O(2^n) in worst case.
-This implementation is practical for n <= 1000 or more. lmao.
-*/
-
 #include<bits/stdc++.h>
 using namespace std;
 
-const int N = 3e5 + 9;
+const int N = 2e5 + 9;
 
-// positive literal  x in [0,n),
-// negative literal ~x in [-n,0)
-// 0 indexed
-struct SAT_GOD {
-  int n;
-  vector<int> occ, pos, neg;
-  vector<vector<int>> g, lit;
-  SAT_GOD(int n) : n(n), g(2*n), occ(2*n) { }
-  void add_clause(const vector<int> &c) {
-    for(auto u: c) {
-      g[u+n].push_back(lit.size());
-      occ[u+n] += 1;
-    }
-    lit.push_back(c);
+int n;
+vector<pair<int, int>> g[N];
+struct PT {
+  int x, y, id;
+  bool operator < (const PT &p) const {
+    return x == p.x ? y < p.y : x < p.x;
   }
-  //(!u | v | !w) -> (u, 0, v, 1, w, 0)
-  void add(int u, int af, int v = 1e9, int bf = 0, int w = 1e9, int cf = 0) {
-    vector<int> a;
-    if(!af) u = ~u;
-    a.push_back(u);
-    if(v != 1e9) {
-      if(!bf) v = ~v;
-      a.push_back(v);
-    }
-    if(w != 1e9) {
-      if(!cf) w = ~w;
-      a.push_back(w);
-    }
-    add_clause(a);
-  }
-  vector<bool> x;
-  vector<vector<int>> decision_stack;
-  vector<int> unit_stack, pure_stack;
-  void push(int u) {
-    x[u + n] = 1;
-    decision_stack.back().push_back(u);
-    for (auto i: g[u + n]) if (pos[i]++ == 0) {
-        for (auto u: lit[i]) --occ[u+n];
-      }
-    for (auto i: g[~u + n]) {
-      ++neg[i];
-      if (pos[i] == 0) unit_stack.push_back(i);
-    }
-  }
-  void pop() {
-    int u = decision_stack.back().back();
-    decision_stack.back().pop_back();
-    x[u + n] = 0;
-    for (auto i: g[u + n]) if (--pos[i] == 0) {
-        for (auto u: lit[i]) ++occ[u + n];
-      }
-    for (auto i: g[~u+n]) --neg[i];
-  }
-  bool reduction() {
-    while(!unit_stack.empty() || !pure_stack.empty()) {
-      if(!pure_stack.empty()) {  // pure literal elimination
-        int u = pure_stack.back();
-        pure_stack.pop_back();
-        if (occ[u + n] == 1 && occ[~u + n] == 0) push(u);
-      } else {                   // unit propagation
-        int i = unit_stack.back();
-        unit_stack.pop_back();
-        if(pos[i] > 0) continue;
-        if(neg[i]     == lit[i].size()) return false;
-        if(neg[i] + 1 == lit[i].size()) {
-          int w = n;
-          for (int u: lit[i]) if (!x[u + n] && !x[~u + n]) w = u;
-          if (x[~w + n]) return false;
-          push(w);
-        }
-      }
-    }
-    return true;
-  }
-  bool ok() {
-    x.assign(2*n,0);
-    pos = neg = vector<int>(lit.size());
-    decision_stack.assign(1, {});
-    while(1) {
-      if(reduction()) {
-        int s = 0;
-        for(int u = 0; u < n; ++u) if(occ[s + n] + occ[~s + n] < occ[u + n] + occ[~u + n]) s = u;
-        if(occ[s + n] + occ[~s + n] == 0) return true;
-        decision_stack.push_back({});
-        push(s);
-      } else {
-        int s = decision_stack.back()[0];
-        while(!decision_stack.back().empty()) pop();
-        decision_stack.pop_back();
-        if (decision_stack.empty()) return false;
-        push(~s);
-      }
-    }
-  }
+} p[N];
+struct node {
+  int val, id;
+} t[N];
+struct DSU {
+  int p[N];
+  void init(int n) { for (int i = 1; i <= n; i++)  p[i] = i; }
+  int find(int u) { return p[u] == u ? u : p[u] = find(p[u]); }
+  void merge(int u, int v) { p[find(u)] = find(v); }
+} dsu;
+struct edge {
+  int u, v, w;
+  bool operator < (const edge &p) const { return w < p.w; }
 };
-
-int32_t main() {
-  int n = 9;
-  SAT_GOD t(n);
-  t.add(0, 0, 1, 1);
-  t.add(1, 0);
-  t.add(1, 0, 3, 1, 5, 1);
-  cout << t.ok() << endl;
+vector<edge> edges;
+int query(int x) {
+  int r = 2e9 + 10, id = -1;
+  for (; x <= n; x += (x & -x)) if (t[x].val < r) r = t[x].val, id = t[x].id;
+  return id;
 }
+void modify(int x, int w, int id) {
+  for (; x > 0; x -= (x & -x)) if (t[x].val > w) t[x].val = w, t[x].id = id;
+}
+int dist(PT &a, PT &b) {
+  return abs(a.x - b.x) + abs(a.y - b.y);
+}
+void add(int u, int v, int w) {
+  edges.push_back({u, v, w});
+}
+long long Kruskal() {
+  dsu.init(n);
+  sort(edges.begin(), edges.end());
+  long long ans = 0;
+  for (edge e : edges) {
+    int u = e.u, v = e.v, w = e.w;
+    if (dsu.find(u) != dsu.find(v)) {
+      ans += w;
+      g[u].push_back({v, w});
+      //g[v].push_back({u, w});
+      dsu.merge(u, v);
+    }
+  }
+  return ans;
+}
+void Manhattan() {
+  for (int i = 1; i <= n; ++i) p[i].id = i;
+  for (int dir = 1; dir <= 4; ++dir) {
+    if (dir == 2 || dir == 4) {
+      for (int i = 1; i <= n; ++i) swap(p[i].x, p[i].y);
+    } 
+    else if (dir == 3) {
+      for (int i = 1; i <= n; ++i) p[i].x = -p[i].x;
+    }
+    sort(p + 1, p + 1 + n);
+    vector<int> v;
+    static int a[N];
+    for (int i = 1; i <= n; ++i) a[i] = p[i].y - p[i].x, v.push_back(a[i]);
+    sort(v.begin(), v.end());
+    v.erase(unique(v.begin(), v.end()), v.end());
+    for (int i = 1; i <= n; ++i) a[i] = lower_bound(v.begin(), v.end(), a[i]) - v.begin() + 1;
+    for (int i = 1; i <= n; ++i) t[i].val = 2e9 + 10, t[i].id = -1;
+    for (int i = n; i >= 1; --i) {
+      int pos = query(a[i]);
+      if (pos != -1) add(p[i].id, p[pos].id, dist(p[i], p[pos]));
+      modify(a[i], p[i].x + p[i].y, i);
+    }
+  }
+}
+int32_t main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cin >> n;
+  for (int i = 1; i <= n; i++) cin >> p[i].x >> p[i].y;
+  Manhattan();
+  cout << Kruskal() << '\n';
+  for (int u = 1; u <= n; u++) {
+    for (auto x: g[u]) cout << u - 1 << ' ' << x.first - 1 << '\n';
+  }
+  return 0;
+}
+// https://judge.yosupo.jp/problem/manhattanmst
