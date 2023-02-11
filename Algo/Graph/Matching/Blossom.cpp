e2456579d7af6bc3e03ebb6a464cef709192af86
João Victor Malheiros Farias de Aquino <38818400+Malhe1ros@users.noreply.github.com>
2023-02-11 16:19:04 -0300

    Update Blossom.cpp

--- Algo/Graph/Matching/Blossom.cpp
+++ Algo/Graph/Matching/Blossom.cpp
@@ -1,161 +1,127 @@
-#include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
 
-const int    N = 500000;
-int          n;
-vector<int>  edges[N];
 
-int lca( vector<int> &match, vector<int> &base, vector<int> &p,
-         int a, int b ) 
-{
-    vector<bool>  used( match.size() );
+const int MAX = 2010;
+vector<int> g[MAX];
+int match[MAX]; // match[i] = com quem i esta matchzado ou -1
+int n, pai[MAX], base[MAX], vis[MAX]; //tem que setar o n
+queue<int> q;
 
-    while( true )  {
-        a = base[a];
-        used[a] = true;
-        if( match[a] == -1 )   break;
-        a = p[match[a]];
-    }
-
-    while( true )  {
-        b = base[b];
-        if( used[b] )   return b;
-        b = p[match[b]];
-    }
+void contract(int u, int v, bool first = 1) {
+	static vector<bool> bloss;
+	static int l;
+	if (first) {
+		bloss = vector<bool>(n, 0);
+		vector<bool> teve(n, 0);
+		int k = u; l = v;
+		while (1) {
+			teve[k = base[k]] = 1;
+			if (match[k] == -1) break;
+			k = pai[match[k]];
+		}
+		while (!teve[l = base[l]]) l = pai[match[l]];
+	}
+	while (base[u] != l) {
+		bloss[base[u]] = bloss[base[match[u]]] = 1;
+		pai[u] = v;
+		v = match[u];
+		u = pai[match[u]];
+	}
+	if (!first) return;
+	contract(v, u, 0);
+	for (int i = 0; i < n; i++) if (bloss[base[i]]) {
+		base[i] = l;
+		if (!vis[i]) q.push(i);
+		vis[i] = 1;
+	}
 }
 
-void markPath( vector<int> &match, vector<int> &base, vector<bool> &blossom,
-               vector<int> &p, int v, int b, int children ) 
-{
-     for( ; base[v]!=b ; v=p[match[v]] )  {
-          blossom[base[v]] = blossom[base[match[v]]] = true;
-          p[v] = children;
-          children = match[v];
-     }
+int getpath(int s) {
+	for (int i = 0; i < n; i++) base[i] = i, pai[i] = -1, vis[i] = 0;
+	vis[s] = 1; q = queue<int>(); q.push(s);
+	while (q.size()) {
+		int u = q.front(); q.pop();
+		for (int i : g[u]) {
+			if (base[i] == base[u] or match[u] == i) continue;
+			if (i == s or (match[i] != -1 and pai[match[i]] != -1))
+				contract(u, i);
+			else if (pai[i] == -1) {
+				pai[i] = u;
+				if (match[i] == -1) return i;
+				i = match[i];
+				vis[i] = 1; q.push(i);
+			}
+		}
+	}
+	return -1;
 }
 
-int findPath( vector<int> &match, vector<int> &p, int root ) 
-{
-    vector<bool>  used( n );
-    for( int i=0 ; i<p.size() ; ++i )   p[i] = -1;
-
-    vector<int>  base( n );
-    for( int i=0 ; i<n ; ++i )   base[i] = i;
-    used[root] = true;
-    int  qh = 0;  int qt = 0;
-    vector<int>  q( n );
-    q[qt++] = root;
-    while( qh < qt )  {
-        int v = q[qh++];
-        for( int to : edges[v] )  {
-            if( base[v] == base[to]  ||  match[v] == to )
-	            continue;
-            if( to == root  ||  match[to] != -1  &&
-	            p[match[to]] != -1 )  
-            {
-                int  curbase = lca( match, base, p, v, to );
-                vector<bool>  blossom( n );
-                markPath( match, base, blossom, p, v, curbase, to );
-                markPath( match, base, blossom, p, to, curbase, v );
-                for( int i=0 ; i<n ; ++i )
-                    if( blossom[base[i]] )  {
-                        base[i] = curbase;
-                        if( !used[i] )  {
-                            used[i] = true;
-                            q[qt++] = i;
-                        }
-                    }
-            }
-            else if( p[to] == -1 ) 
-            {
-                p[to] = v;
-                if( match[to] == -1 )   return to;
-                to = match[to];
-                used[to] = true;
-                q[qt++] = to;
-            }
-        }
-    }
-    return -1;
+int blossom() {
+	int ans = 0;
+	memset(match, -1, sizeof(match));
+	for (int i = 0; i < n; i++) if (match[i] == -1)
+		for (int j : g[i]) if (match[j] == -1) {
+			match[i] = j;
+			match[j] = i;
+			ans++;
+			break;
+		}
+	for (int i = 0; i < n; i++) if (match[i] == -1) {
+		int j = getpath(i);
+		if (j == -1) continue;
+		ans++;
+		while (j != -1) {
+			int p = pai[j], pp = match[p];
+			match[p] = j;
+			match[j] = p;
+			j = pp;
+		}
+	}
+	return ans;
 }
-
-vector< pair<int,int> > maxMatching() 
-{
-    vector<int>  match( n, -1 ),  p( n );
-
-    for( int i=0 ; i<n ; ++i )  {
-        if( match[i] == -1 )  {
-            int  v = findPath( match, p, i );
-            while( v != -1 )  {
-                int  pv  = p[v];
-                int  ppv = match[pv];
-                match[v] = pv;
-                match[pv] = v;
-                v = ppv;
-            }
-        }
-    }
-
-    vector< pair<int,int> >  thePath;
-
-    for( int i=0 ; i<n ; ++i )
-        if( match[i] != -1 )
-            thePath.emplace_back( i, match[i] );
-
-    return thePath;
-}
-
-void addEdge(int u,int v){
-    edges[u].push_back(v);
-    edges[v].push_back(u);
-}
-
+const int maxn  = 1e5+10;
+int gamb[maxn];
 signed main(){
     cin.tie(NULL);
-    cin.sync_with_stdio(false);
-    cin>>n;
-    vector<string> v(n);
-    vector<int> teste(n);
-    vector<int> valor(n);
-    for(int i=0;i<n;i++){
-        string a;cin>>v[i]>>a>>valor[i];
-        if(a[0]=='a')teste[i]=2;
-        else if(a[0]=='s')teste[i]=0;
-        else teste[i]=1;
+    ios_base::sync_with_stdio(false);
+    int m;cin>>n>>m;
+    vector<int>v;
+    for(int i = 0;i<m;i++){
+        int a;cin>>a;
+        v.push_back(a);
     }
-    for(int i=0;i<n;i++){
-        for(int j=i+1;j<n;j++){
-            if(teste[i]==0){//antes
-                if(abs(valor[i]-valor[j])==2){
-                    if(teste[j]!=0){
-                        addEdge(i,j);
-                    }
-                }
-            }
-            else if(teste[i]==1){
-                if(abs(valor[i]-valor[j])==2){
-                    if(teste[j]!=1){
-                        addEdge(i,j);
-                    }
-                }             
-            }
-            else{
-                if(abs(valor[i]-valor[j])==2){
-                    addEdge(i,j);
-                }
-            }
+    vector<vector<int>> caras(n);
+    for(int i = 0;i<n;i++){
+        int x;cin>>x;
+        for(int j = 0;j<x;j++){
+            int a;cin>>a;
+            caras[i].push_back(a);
+        }
+
+    }
+    auto pode = [&](int a,int b){
+        for(auto k:caras[a])gamb[k]++;
+        for(auto k:caras[b])gamb[k]++;
+        bool dm = 1;
+        for(auto k:v){
+            if(gamb[k]==0)dm = 0;
+        }
+        for(auto k:caras[a])gamb[k]--;
+        for(auto k:caras[b])gamb[k]--;
+        return dm;
+    };
+    for(int i = 0;i<n;i++){
+        for(int j = i+1;j<n;j++)if(pode(i,j)){
+            g[i].push_back(j);
+            g[j].push_back(i);
+            //cout<<i<<" "<<j<<endl;
         }
     }
-    auto ans=maxMatching();
-    cout<<(ans.size())/2<<endl;
-    for(auto k:ans){
-        if(k.first<k.second){
-            //cout<<k.first<<" "<<k.second<<endl;
-            if((teste[k.first]==0) || (teste[k.second]==1)){
-                cout<<v[k.first]<<" "<<v[k.second]<<'\n';
-            }
-            else cout<<v[k.second]<<" "<<v[k.first]<<'\n';
-        }
+    auto ans = blossom();
+    //cout<<ans<<endl;
+    if(2*ans==n){
+        cout<<"SIM\n";
     }
+    else cout<<"NAO\n";
 }
