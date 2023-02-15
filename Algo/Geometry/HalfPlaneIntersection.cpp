41a780a75f0535044c3933988c6bef3627591c39
João Victor Malheiros Farias de Aquino <38818400+Malhe1ros@users.noreply.github.com>
2023-02-15 16:21:04 -0300

    Create HalfPlaneIntersection.cpp

---
+++ Algo/Geometry/HalfPlaneIntersection.cpp
@@ -0,0 +1,172 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Redefine epsilon and infinity as necessary. Be mindful of precision errors.
+const long double eps = 1e-9, inf = 1e9; 
+
+// Basic point/vector struct.
+struct Point { 
+
+    long double x, y;
+    explicit Point(long double x = 0, long double y = 0) : x(x), y(y) {}
+
+    // Addition, substraction, multiply by constant, dot product, cross product.
+
+    friend Point operator + (const Point& p, const Point& q) {
+        return Point(p.x + q.x, p.y + q.y); 
+    }
+
+    friend Point operator - (const Point& p, const Point& q) { 
+        return Point(p.x - q.x, p.y - q.y); 
+    }
+
+    friend Point operator * (const Point& p, const long double& k) { 
+        return Point(p.x * k, p.y * k); 
+    } 
+
+    friend long double dot(const Point& p, const Point& q) {
+        return p.x * q.x + p.y * q.y;
+    }
+
+    friend long double cross(const Point& p, const Point& q) { 
+        return p.x * q.y - p.y * q.x; 
+    }
+};
+
+// Basic half-plane struct.
+struct Halfplane { 
+
+    // 'p' is a passing point of the line and 'pq' is the direction vector of the line.
+    Point p, pq; 
+    long double angle;
+
+    Halfplane() {}
+    Halfplane(const Point& a, const Point& b) : p(a), pq(b - a) {
+        angle = atan2l(pq.y, pq.x);    
+    }
+
+    // Check if point 'r' is outside this half-plane. 
+    // Every half-plane allows the region to the LEFT of its line.
+    bool out(const Point& r) { 
+        return cross(pq, r - p) < -eps; 
+    }
+
+    // Comparator for sorting. 
+    bool operator < (const Halfplane& e) const { 
+        return angle < e.angle;
+    } 
+
+    // Intersection point of the lines of two half-planes. It is assumed they're never parallel.
+    friend Point inter(const Halfplane& s, const Halfplane& t) {
+        long double alpha = cross((t.p - s.p), t.pq) / cross(s.pq, t.pq);
+        return s.p + (s.pq * alpha);
+    }
+};
+
+
+// Actual algorithm
+vector<Point> hp_intersect(vector<Halfplane>& H) { 
+
+    Point box[4] = {  // Bounding box in CCW order
+        Point(inf, inf), 
+        Point(-inf, inf), 
+        Point(-inf, -inf), 
+        Point(inf, -inf) 
+    };
+
+    for(int i = 0; i<4; i++) { // Add bounding box half-planes.
+        Halfplane aux(box[i], box[(i+1) % 4]);
+        H.push_back(aux);
+    }
+
+    // Sort by angle and start algorithm
+    sort(H.begin(), H.end());
+    deque<Halfplane> dq;
+    int len = 0;
+    for(int i = 0; i < int(H.size()); i++) {
+
+        // Remove from the back of the deque while last half-plane is redundant
+        while (len > 1 && H[i].out(inter(dq[len-1], dq[len-2]))) {
+            dq.pop_back();
+            --len;
+        }
+
+        // Remove from the front of the deque while first half-plane is redundant
+        while (len > 1 && H[i].out(inter(dq[0], dq[1]))) {
+            dq.pop_front();
+            --len;
+        }
+
+        // Special case check: Parallel half-planes
+        if (len > 0 && fabsl(cross(H[i].pq, dq[len-1].pq)) < eps) {
+            // Opposite parallel half-planes that ended up checked against each other.
+            if (dot(H[i].pq, dq[len-1].pq) < 0.0)
+                return vector<Point>();
+
+            // Same direction half-plane: keep only the leftmost half-plane.
+            if (H[i].out(dq[len-1].p)) {
+                dq.pop_back();
+                --len;
+            }
+            else continue;
+        }
+
+        // Add new half-plane
+        dq.push_back(H[i]);
+        ++len;
+    }
+
+    // Final cleanup: Check half-planes at the front against the back and vice-versa
+    while (len > 2 && dq[0].out(inter(dq[len-1], dq[len-2]))) {
+        dq.pop_back();
+        --len;
+    }
+
+    while (len > 2 && dq[len-1].out(inter(dq[0], dq[1]))) {
+        dq.pop_front();
+        --len;
+    }
+
+    // Report empty intersection if necessary
+    if (len < 3) return vector<Point>();
+
+    // Reconstruct the convex polygon from the remaining half-planes.
+    vector<Point> ret(len);
+    for(int i = 0; i+1 < len; i++) {
+        ret[i] = inter(dq[i], dq[i+1]);
+    }
+    ret.back() = inter(dq[len-1], dq[0]);
+    return ret;
+}
+
+long double area(const vector<Point>& fig) {
+    long double res = 0;
+    for (unsigned i = 0; i < fig.size(); i++) {
+        Point p = i ? fig[i - 1] : fig.back();
+        Point q = fig[i];
+        res += (p.x - q.x) * (p.y + q.y);
+    }
+    return abs(res) / 2;
+}
+
+signed main(){
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    vector<Halfplane> hps;
+    vector<Point> v;
+    int n;cin>>n;
+    for(int i=0;i<n;i++){
+        int a,b;cin>>a>>b;
+        Point p(a,b);
+        v.push_back(p);
+    }
+    v.push_back(v[0]);
+    for(int i=1;i<=n;i++){
+        hps.push_back(Halfplane(v[i],v[i-1]));
+    }
+    auto ans = hp_intersect(hps);
+    //cout<<"sai"<<endl;
+    cout<<fixed<<setprecision(9);
+    cout<<area(ans)<<'\n';
+}
