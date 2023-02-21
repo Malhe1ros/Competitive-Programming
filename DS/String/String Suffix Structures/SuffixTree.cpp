7c555d60db1ba808da311cb08115ce40f475c323
João Victor Malheiros Farias de Aquino <38818400+Malhe1ros@users.noreply.github.com>
2023-02-20 21:37:39 -0300

    Update SuffixTree.cpp

--- DS/String/String Suffix Structures/SuffixTree.cpp
+++ DS/String/String Suffix Structures/SuffixTree.cpp
@@ -2,8 +2,16 @@
  
 using namespace std;
  #define int long long
+/*
+    Bom saber:
+        Quantidade de substrings distintas:
+        O que é? Cada path da raiz pra algum canto é uma substring distinta, simplesmente a quantidade de paths + desconsiderar o $
+        Cálculo = soma de sf::t[u].len() - (s.size() + 1)
+        
+        
+*/
 namespace sf {
-  const int NS = 1;
+  const int NS = 1;//numero de strings que vou colocar
   const int N =  500010*2;
 
   int cn, cd, ns, en = 1, lst;
@@ -17,13 +25,14 @@ namespace sf {
     //r = fim da substring do edge acima;
     //si = de qual string faz parte acima;
     int p, suf;
+    int qbad = 0;
     //p = pai;
     //suf = suffix link;
     map<char, int> adj;
     node() : l(0), r(-1), suf(0), p(0) {}
     node(int L, int R, int S, int P) : l(L), r(R), si(S), p(P) {}
-    inline int len() { return r - l + 1; }
-    inline int operator[](int i) { return S[si][l + i]; }
+    inline int len() { return r - l + 1; } //length do edge acima;
+    inline int operator[](int i) { return S[si][l + i]; } //getKth do edge acima;
     inline int& operator()(char c) { return adj[c]; }
   } t[N];
 
@@ -81,15 +90,50 @@ namespace sf {
   }
 };
 
+/*
+    Para esse problema, me retorne todas as substrings distintas que tem menos de K caracteres ruins
+    DP na suffix tree;
+*/
 int tot=0;
-void dfs(int u){
-  tot+=sf::t[u].len();
-  for(auto k:sf::t[u].adj)dfs(k.second);
+int bad[26];
+int k;
+void dfs(int u,int p){
+    if(p!=-1){
+        sf::t[u].qbad = sf::t[p].qbad;//pego info do pai
+        int l = sf::t[u].l;
+        int r = sf::t[u].r;
+        for(int i = l;i<=r;i++){//o intervalo do meu edge
+            char qual = sf::t[u][i-l];//caracter naquela posição
+            if(qual=='$')continue;
+            if(bad[qual-'a']){//se for ruim aumento a DP
+                sf::t[u].qbad++;
+            }
+
+            if(sf::t[u].qbad<=k)tot++;//se valido, conto
+
+        }
+    }
+
+    for(auto K:sf::t[u].adj){ //dfs na suffix tree
+        dfs(K.second,u);
+    }
+
 }
 
 signed main(){
-	string s;cin>>s;
-  sf::add_string(s);
-  dfs(0);
-  cout<<tot-s.length()-1<<endl;
+	
+    string s;cin>>s;
+    sf::add_string(s);
+    
+    string s2;cin>>s2;
+    for(int i=0;i<26;i++){
+        bad[i] = s2[i]-'0';
+        bad[i]^=1;
+    }
+    cin>>k;
+
+    dfs(0,-1);
+    
+    cout<<tot<<endl;
+    //cout<<tot- (s.size()+1)<<endl;
 }
